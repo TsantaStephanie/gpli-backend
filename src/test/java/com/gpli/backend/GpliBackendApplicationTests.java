2c15b1b56ad78defd0189555c722efff482cd88b
Tsanta <tsantarakotoarisoa620@gmail.com>
2026-06-05 09:22:10 +0100

    Initial commit

---
+++ src/test/java/com/gpli/backend/GpliBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpli.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpliBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
